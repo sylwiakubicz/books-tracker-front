8960a506f918e9764a0c7d8af705081aa606aade
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-07-24 12:00:34 +0200

    Dodano ustawienia związane z Tailwind css. Przyogotowano projekt pod dalszą pracę.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      "font-montserrat": ["Montserrat", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
