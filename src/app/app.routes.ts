816103665b30080137216121700905c68c4208f8
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-07-24 13:52:21 +0200

    Poprawiono ustawienia routingu.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginPageComponent} from "./login-page/login-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginPageComponent}
+];
+
