13931e529da941a8e391202ed8ac2e9ef427ead3
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-07-29 22:22:54 +0200

    Komponent korzysta z wcześniej utworzonych submit-btn oraz form-input komponentów. Zdefiniowano metodę, która będzie wykonywana podczas przesłania formularza.

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {SubmitBtnComponent} from "../submit-btn/submit-btn.component";
+import {FormInputComponent} from "../form-input/form-input.component";
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [
+    SubmitBtnComponent,
+    FormInputComponent
+  ],
   templateUrl: './login-form.component.html',
 })
 export class LoginFormComponent {
-
+  handleSignIn() {
+    console.log('Sign In button clicked');
+  }
 }
