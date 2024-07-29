b1526d0b3461e996b14a57e4657412d0fc31edba
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-07-29 22:18:41 +0200

    Komponent korzysta z wcześniej utworzonych submit-btn oraz form-input komponentów. Zdefiniowano metodę, która będzie wykonywana podczas przesłania formularza.

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {FormInputComponent} from "../form-input/form-input.component";
+import {SubmitBtnComponent} from "../submit-btn/submit-btn.component";
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormInputComponent,
+    SubmitBtnComponent
+  ],
   templateUrl: './register-form.component.html',
 })
 export class RegisterFormComponent {
 
+  handleSignUp() {
+    console.log("sign up")
+  }
 }
