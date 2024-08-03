e4b1f1c660abdd3b35f41b47d5f7ac191750d9c4
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-03 10:37:05 +0200

    Dodano domyślną trasę w konfiguracji routingu.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {RegisterPageComponent} from "./register-page/register-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 export const routes: Routes = [
+  {path: "", component: HomePageComponent},
   {path: 'login', component: LoginPageComponent},
   {path: 'register', component: RegisterPageComponent}
 ];
