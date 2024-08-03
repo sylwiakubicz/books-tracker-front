7b004788f43b647d2cf2d2e8c53884108efb40d1
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-03 10:53:47 +0200

    Dodano serwis 'BooksService' wraz z testową metodą. Metoda miała na celu sprawdzenie czy credentials po logowaniu są przekazywane poprawnie.

---
+++ src/services/BooksService.ts
@@ -0,0 +1,21 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  constructor(private http: HttpClient) {}
+
+  Test(): Observable<any> {
+    return this.http.get("http://localhost:8080/books/get", { withCredentials: true }).pipe(
+      map((response) => {
+        console.log("Response received:", response);
+        return response;
+      })
+    );
+  }
+
+}
