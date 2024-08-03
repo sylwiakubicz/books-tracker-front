76609a52d312dd2b1e59881fc4d8cc93866234e2
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-03 10:53:11 +0200

    Dodano konfigurachę interceptora do aplikacji.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import {AuthInterceptor} from "../services/AuthInterceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-    provideHttpClient()
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 };
