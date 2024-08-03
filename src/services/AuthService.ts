6ae2408d0dd67871304a59a5ae7d53dcd42c6c50
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-03 10:39:45 +0200

    Dodano metodę Login odpowiadającą za wykonanie żądania na endpoint http://localhost:8080/api/auth/login służący do logowania użytkownika.

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
-import { map } from "rxjs/operators";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,17 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   Register(username: string, email: string, password: string): Observable<any> {
-    return this.http.post("http://localhost:8080/api/auth/register", { username, email, password }).pipe(
+    return this.http.post("http://localhost:8080/api/auth/register", { username, email, password }, { withCredentials: true }).pipe(
+      map((response) => {
+        console.log("Response received:", response);
+        return response;
+      })
+    );
+  }
+
+  Login(username: string, password: string): Observable<any> {
+    console.log("logging...")
+    return this.http.post("http://localhost:8080/api/auth/login", { username, password }, { withCredentials: true }).pipe(
       map((response) => {
         console.log("Response received:", response);
         return response;
