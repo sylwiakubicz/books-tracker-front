9451dc75e08a0500715aa1835fb366d5f0fa464f
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-03 10:48:30 +0200

    Utworzono metodę login i obsługę błędów w komponencie logowania.

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,12 +1,16 @@
 import {Component} from '@angular/core';
 import {SubmitBtnComponent} from "../submit-btn/submit-btn.component";
 import {FormsModule} from "@angular/forms";
+import {AuthService} from "../../services/AuthService";
+import {Router} from "@angular/router";
+import {ErrorDisplayComponent} from "../error-display/error-display.component";
 
 
 @Component({
   imports: [
     SubmitBtnComponent,
-    FormsModule
+    FormsModule,
+    ErrorDisplayComponent
   ],
   selector: 'app-login-form',
   standalone: true,
@@ -14,21 +18,35 @@ import {FormsModule} from "@angular/forms";
 })
 
 export class LoginFormComponent {
-  loginObj : LoginData;
+  loginObj: LoginData;
+  error: string | null = null;
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
     this.loginObj = new LoginData();
   }
 
+  login() {
+    this.authService.Login(this.loginObj.username, this.loginObj.password).subscribe({
+      next: (response) => {
+        this.error = null;
+        this.router.navigate(['/']); // Navigate to home or another route on successful login
+      },
+      error: (err) => {
+        this.error = err.error.message;
+      }
+    });
+  }
+
   handleSignIn() {
-    console.log(this.loginObj)
+    console.log(this.loginObj);
     console.log('Sign In button clicked');
+    this.login();
   }
 }
 
 export class LoginData {
-  username: String;
-  password : String;
+  username: string;
+  password: string;
 
   constructor() {
     this.username = '';
