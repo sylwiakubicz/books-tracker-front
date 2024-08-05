85a14d4cc54ad1ce35a1807a4fd7befd98fc00ed
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-05 23:41:53 +0200

    Utworzono book-card komponent.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {SubmitBtnComponent} from "../submit-btn/submit-btn.component";
 import {BooksService} from "../../services/BooksService";
 import {NavbarComponent} from "../navbar/navbar.component";
+import {BookCardComponent} from "../book-card/book-card.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     SubmitBtnComponent,
-    NavbarComponent
+    NavbarComponent,
+    BookCardComponent
   ],
   templateUrl: './home-page.component.html',
 })
