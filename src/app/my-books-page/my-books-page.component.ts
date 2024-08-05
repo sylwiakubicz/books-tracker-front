a9a18edcbfb5d5901ad277f3401a842785fb2db4
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-05 22:17:14 +0200

    Utworzono komponent, który będzie wyświetlany na ścieżce 'mybooks'.

---
+++ src/app/my-books-page/my-books-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-books-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-books-page.component.html',
+  styles: ``
+})
+export class MyBooksPageComponent {
+
+}
