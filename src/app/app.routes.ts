d98f8fad941852439e4026d9dc80c542862ffc2b
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-05 22:17:49 +0200

    Dodano nową ścieżkę do konfiguracji routingu wskazującą na nowy komponent 'MyBooksPageComponent'.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import {Routes} from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {RegisterPageComponent} from "./register-page/register-page.component";
 import {HomePageComponent} from "./home-page/home-page.component";
+import {MyBooksPageComponent} from "./my-books-page/my-books-page.component";
 
 export const routes: Routes = [
-  {path: "", component: HomePageComponent},
+  {
+    path: "",
+    "component": HomePageComponent,
+    children: [
+      {path: "mybooks", component: MyBooksPageComponent}
+  ]},
   {path: 'login', component: LoginPageComponent},
   {path: 'register', component: RegisterPageComponent}
 ];
