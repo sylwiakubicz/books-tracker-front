1e88261a16d9fb84ab68606d6cc10522102dbb0d
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-10 20:04:32 +0200

    Utworzono star-rating komponent.

--- src/app/book-page/book-page.component.ts
+++ src/app/book-page/book-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {faStar} from "@fortawesome/free-regular-svg-icons/faStar";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {StarRatingComponent} from "../star-rating/star-rating.component";
 
 @Component({
   selector: 'app-book-page',
   standalone: true,
   imports: [
-    FaIconComponent
+    FaIconComponent,
+    StarRatingComponent
   ],
   templateUrl: './book-page.component.html',
 })
---
+++ src/app/star-rating/star-rating.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, input} from '@angular/core';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {faStar as farStar} from "@fortawesome/free-regular-svg-icons/faStar";
+import {faStar} from "@fortawesome/free-solid-svg-icons/faStar";
+
+@Component({
+  selector: 'app-star-rating',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './star-rating.component.html',
+  styleUrl: './star-rating.component.css'
+})
+export class StarRatingComponent {
+  protected readonly farStar = farStar;
+  protected readonly faStar = faStar;
+
+  @Input() rating = 0;
+
+  setRating (value : number) {
+    this.rating = value;
+  }
+}
