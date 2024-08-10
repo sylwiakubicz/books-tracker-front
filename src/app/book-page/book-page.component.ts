b18cbdb8917fee29ff87ef736ff6ec9aa7c52786
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-10 17:37:37 +0200

    Ustawiono wygląd book-page komponent na najmniejszym ekranie.

--- src/app/book-page/book-page.component.ts
+++ src/app/book-page/book-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {faStar} from "@fortawesome/free-regular-svg-icons/faStar";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-book-page',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './book-page.component.html',
-  styles: ``
 })
 export class BookPageComponent {
 
+  protected readonly farStar = faStar;
 }
