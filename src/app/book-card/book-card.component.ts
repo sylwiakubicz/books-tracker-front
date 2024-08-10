b883fae8b1c054014e7b30611728b031b019f151
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-10 16:36:11 +0200

    Utworzono book-page komponent i dodano na niego przekierowanie po kliknięciu w tytuł książki.

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faStar} from "@fortawesome/free-solid-svg-icons/faStar";
 import {faStar as farStar} from "@fortawesome/free-regular-svg-icons/faStar";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-book-card',
   standalone: true,
   imports: [
-    FaIconComponent
+    FaIconComponent,
+    RouterLink
   ],
   templateUrl: './book-card.component.html',
   styles: ``
---
+++ src/app/book-page/book-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-page.component.html',
+  styles: ``
+})
+export class BookPageComponent {
+
+}
