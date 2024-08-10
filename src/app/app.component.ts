3158da53545cc7bd88d2a0a79be9375a7f09de85
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-10 16:31:27 +0200

    Dodano navbar nad router-outlet, żeby każzdy komponent miał dostęp do navabru.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent, FontAwesomeModule],
+  imports: [RouterOutlet, LoginPageComponent, FontAwesomeModule, NavbarComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
