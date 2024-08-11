d3b0232ada1d3fd296e466d14bc78d51e1af6d5f
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-11 23:14:23 +0200

    Dodano nową sekcję do book-page komponentu z informacjami o książce. Dostosowano wygląd tylko na ekran mobilny.

--- src/app/book-page/book-page.component.ts
+++ src/app/book-page/book-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {StarRatingComponent} from "../star-rating/star-rating.component";
 import {StatusBtnComponent} from "../status-btn/status-btn.component";
+import {BannerComponent} from "../banner/banner.component";
 
 @Component({
   selector: 'app-book-page',
@@ -9,7 +10,8 @@ import {StatusBtnComponent} from "../status-btn/status-btn.component";
   imports: [
     FaIconComponent,
     StarRatingComponent,
-    StatusBtnComponent
+    StatusBtnComponent,
+    BannerComponent
   ],
   templateUrl: './book-page.component.html',
 })
