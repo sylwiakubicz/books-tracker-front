0947826dba0ed1cb8c6db87e41659212f99143a9
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-11 21:28:33 +0200

    Utworzono przycisk pozwalający wybrać status dla danej książki.

--- src/app/status-btn/status-btn.component.ts
+++ src/app/status-btn/status-btn.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {SubmitBtnComponent} from "../submit-btn/submit-btn.component";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-status-btn',
   standalone: true,
-  imports: [],
+  imports: [
+    SubmitBtnComponent,
+    FaIconComponent
+  ],
   templateUrl: './status-btn.component.html',
   styles: ``
 })
 export class StatusBtnComponent {
+  status: string = "Want to read";
+  showStatuses : boolean = false;
+
+  handleShowStatuses() {
+    this.showStatuses = !this.showStatuses;
+  }
+
+  handleSetStatus(status : string) {
+    this.status = status;
+    this.showStatuses = false;
+  }
 
 }
