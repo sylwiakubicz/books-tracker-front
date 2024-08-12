42f77701bbf492848e054724767a16784bbb760a
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-12 22:48:16 +0200

    Zmiana ustawienia elementów na stronie z informacjami o pojedynczej książce.

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {StarRatingComponent} from "../star-rating/star-rating.component";
 import {StatusBtnComponent} from "../status-btn/status-btn.component";
+import {InformationCardComponent} from "../information-card/information-card.component";
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-    imports: [
-        StarRatingComponent,
-        StatusBtnComponent
-    ],
+  imports: [
+    StarRatingComponent,
+    StatusBtnComponent,
+    InformationCardComponent
+  ],
   templateUrl: './book-details.component.html',
   styles: ``
 })
---
+++ src/app/information-card/information-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './information-card.component.html',
+  styles: ``
+})
+export class InformationCardComponent {
+
+}
