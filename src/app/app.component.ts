b565f3c0ae65eebafc556740136dda2d7587079b
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-13 19:49:01 +0200

    Dodano prowizorczną stopkę. Drobne poprawki odległości miedzy elementami, usunięcie zaokrąglonych boków na przycisku.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {NavbarComponent} from "./navbar/navbar.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent, FontAwesomeModule, NavbarComponent],
+  imports: [RouterOutlet, LoginPageComponent, FontAwesomeModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
