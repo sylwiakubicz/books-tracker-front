59933ec666a1da347f86a44c238a381c5625c367
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-15 00:16:40 +0200

    Dodano zmienne @Input do komponentu BookCard. Dodano wczytywanie danych do szablonu z zmiennych komponentu.

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faStar} from "@fortawesome/free-solid-svg-icons/faStar";
 import {faStar as farStar} from "@fortawesome/free-regular-svg-icons/faStar";
 import {RouterLink} from "@angular/router";
+import {Author} from "../home-page/home-page.component";
 
 @Component({
   selector: 'app-book-card',
@@ -14,10 +15,29 @@ import {RouterLink} from "@angular/router";
   templateUrl: './book-card.component.html',
   styles: ``
 })
-export class BookCardComponent {
+export class BookCardComponent implements OnInit{
+  @Input() title: string = '';
+  @Input() authors: Author[] = [];
+  @Input() covering: string = '';
+  @Input() description: string = '';
+  @Input() id : number | undefined;
+  url : string = "https://drive.google.com/thumbnail?id="
+  authorsNames: string = ""
   addedToToRead :boolean = true;
 
   protected readonly faStar = faStar;
   protected readonly farStar = farStar;
+
+  ngOnInit() {
+    this.manageReceivedData()
+  }
+
+  manageReceivedData() {
+    this.url = this.url + this.covering;
+
+    this.authorsNames = this.authors?.map((author: Author) =>
+      `${author.name} ${author.surname}`
+    ).join(', ') || '';
+  }
 
 }
