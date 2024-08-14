2283e545987c60d9dae28fd0fb41a25ffac17eeb
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-14 22:10:00 +0200

    Dodano interfejsy `Book`, `Author` i `Genre`. Dodano metodę `getBook` do pobierania danych książki z serwisu. Wprowadzono zmienne `bookData`, `authorsNames`, `genresNames` i `url` do przechowywania i formatowania danych. Zaimplementowano inicjalizację komponentu z pobieraniem danych przy starcie.

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {StarRatingComponent} from "../star-rating/star-rating.component";
 import {StatusBtnComponent} from "../status-btn/status-btn.component";
 import {InformationCardComponent} from "../information-card/information-card.component";
+import {BooksService} from "../../services/BooksService";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-book-details',
@@ -9,11 +11,65 @@ import {InformationCardComponent} from "../information-card/information-card.com
   imports: [
     StarRatingComponent,
     StatusBtnComponent,
-    InformationCardComponent
+    InformationCardComponent,
+    NgOptimizedImage
   ],
   templateUrl: './book-details.component.html',
   styles: ``
 })
-export class BookDetailsComponent {
 
+export class BookDetailsComponent implements OnInit {
+  @Input() id : number = 0;
+  bookData : Book | undefined;
+  authorsNames: string = "";
+  genresNames: string ='';
+  url : string = "https://drive.google.com/thumbnail?id=";
+
+  constructor(private booksService: BooksService) {
+  }
+
+  ngOnInit(): void {
+    this.getBook(this.id);
+  }
+
+  getBook(id : number) {
+    this.booksService.GetBook(id).subscribe({
+      next: (response) => {
+        this.bookData = response;
+        this.authorsNames = this.bookData?.authors?.map((author: Author) =>
+          `${author.name} ${author.surname}`
+        ).join(', ') || '';
+        this.genresNames = this.bookData?.genres?.map((genre: Genre) =>
+          `${genre.name}`
+        ).join(', ') || '';
+        this.url = this.url + this.bookData?.covering;
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+}
+
+export interface Book {
+  bookId: number;
+  title: string;
+  description: string;
+  pageNumber: number;
+  publicationYear: number;
+  ISBN: string;
+  covering: string;
+  authors: Author[];
+  genres: Genre[];
+}
+
+export interface Author {
+  authorId: number;
+  name: string;
+  surname: string;
+}
+
+export interface Genre {
+  genreId: number;
+  name: string;
 }
