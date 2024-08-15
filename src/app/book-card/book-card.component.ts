707b43ea9027d11f664e5a6e72c36bab8535edd4
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-15 16:41:25 +0200

    Dodano metode 'checkIfBookStateExist'. Na podstawie otrzymanych danych ustawiono zmienną status od której zależy 'fa-icon' w template.

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-import {faStar} from "@fortawesome/free-solid-svg-icons/faStar";
-import {faStar as farStar} from "@fortawesome/free-regular-svg-icons/faStar";
+import {faSquareCheck} from "@fortawesome/free-solid-svg-icons/faSquareCheck";
+import {faSquareCheck as farSquareCheck} from "@fortawesome/free-regular-svg-icons/faSquareCheck";
 import {RouterLink} from "@angular/router";
 import {Author} from "../home-page/home-page.component";
+import {BookStatesService} from "../../services/BookStatesService";
 
 @Component({
   selector: 'app-book-card',
@@ -20,16 +21,21 @@ export class BookCardComponent implements OnInit{
   @Input() authors: Author[] = [];
   @Input() covering: string = '';
   @Input() description: string = '';
-  @Input() id : number | undefined;
-  url : string = "https://drive.google.com/thumbnail?id="
-  authorsNames: string = ""
-  addedToToRead :boolean = true;
+  @Input() id : number = 0;
+  url : string = "https://drive.google.com/thumbnail?id=";
+  authorsNames: string = "";
 
-  protected readonly faStar = faStar;
-  protected readonly farStar = farStar;
+  status : string = "";
+
+  protected readonly faSquareCheck = faSquareCheck;
+  protected readonly farSquareCheck = farSquareCheck;
+
+  constructor(private bookStatesService : BookStatesService) {
+  }
 
   ngOnInit() {
     this.manageReceivedData()
+    this.checkIfBookStateExist()
   }
 
   manageReceivedData() {
@@ -38,6 +44,14 @@ export class BookCardComponent implements OnInit{
     this.authorsNames = this.authors?.map((author: Author) =>
       `${author.name} ${author.surname}`
     ).join(', ') || '';
+  }
+
+  checkIfBookStateExist() {
+    this.bookStatesService.CheckIfExistAndThenGet(this.id).subscribe({
+      next: (response) => {
+        this.status = response?.status.statusName;
+      }
+    })
   }
 
 }
