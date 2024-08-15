e7cdfb405260c8e26ee6f292c087b543831b3cfb
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-15 11:41:38 +0200

    Dodano zmienną `id`, która jest zależna od parametru `id` w adresie URL, oraz przekazano ją w szablonie.

--- src/app/book-page/book-page.component.ts
+++ src/app/book-page/book-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {StarRatingComponent} from "../star-rating/star-rating.component";
 import {StatusBtnComponent} from "../status-btn/status-btn.component";
 import {BannerComponent} from "../banner/banner.component";
 import {BookDetailsComponent} from "../book-details/book-details.component";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-book-page',
@@ -17,6 +18,11 @@ import {BookDetailsComponent} from "../book-details/book-details.component";
   ],
   templateUrl: './book-page.component.html',
 })
-export class BookPageComponent {
-
+export class BookPageComponent implements OnInit {
+  id : number = 0;
+  constructor(private route: ActivatedRoute) {
+  }
+  ngOnInit() {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+  }
 }
