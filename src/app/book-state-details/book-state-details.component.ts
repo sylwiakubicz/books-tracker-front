b0470bca624beae9a9304018a2b807d64d32ca7c
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-15 22:39:01 +0200

    Dodanie elementów do book-state-details komponentu. Utworzenie paska postępu, inputów do trackowania postępów w czytanej książce.

--- src/app/book-state-details/book-state-details.component.ts
+++ src/app/book-state-details/book-state-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {StarRatingComponent} from "../star-rating/star-rating.component";
 import {StatusBtnComponent} from "../status-btn/status-btn.component";
 import { DatePipe } from '@angular/common';
@@ -17,7 +17,9 @@ import {FormsModule} from "@angular/forms";
   styleUrl: `./book-state-details.component.css`
 })
 export class BookStateDetailsComponent {
+  @Input() totalPageNumber : number | undefined;
   startDate : Date = new Date();
   endDate : Date = new Date();
+  currentPage : number = 0;
 
 }
