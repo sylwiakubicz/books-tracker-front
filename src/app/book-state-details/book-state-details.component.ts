45556657b43da3fa9997d5b0a11e770e89d87512
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-15 20:17:31 +0200

    Utworzono komponent zawierający dane o statusach książki. Dodano w nim input do wybierania początkowej i końcowej daty czytania.

---
+++ src/app/book-state-details/book-state-details.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnChanges, SimpleChanges} from '@angular/core';
+import {StarRatingComponent} from "../star-rating/star-rating.component";
+import {StatusBtnComponent} from "../status-btn/status-btn.component";
+import { DatePipe } from '@angular/common';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-book-state-details',
+  standalone: true,
+  imports: [
+    StarRatingComponent,
+    StatusBtnComponent,
+    FormsModule,
+    DatePipe
+  ],
+  templateUrl: './book-state-details.component.html',
+  styleUrl: `./book-state-details.component.css`
+})
+export class BookStateDetailsComponent {
+  startDate : Date = new Date();
+  endDate : Date = new Date();
+
+}
