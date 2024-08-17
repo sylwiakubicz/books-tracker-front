6ddd6ac76d1534bed18e97770b60fb924f283cb0
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-17 17:56:04 +0200

    Dodano metodę addBookToNewStatus oraz logikę ustawiania statusu w handleSetStatus.

--- src/app/status-btn/status-btn.component.ts
+++ src/app/status-btn/status-btn.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {SubmitBtnComponent} from "../submit-btn/submit-btn.component";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faSquareCheck as farSquareCheck} from "@fortawesome/free-regular-svg-icons/faSquareCheck";
+import {BookStatesService} from "../../services/BookStatesService";
 
 @Component({
   selector: 'app-status-btn',
@@ -13,14 +14,17 @@ import {faSquareCheck as farSquareCheck} from "@fortawesome/free-regular-svg-ico
   templateUrl: './status-btn.component.html',
   styles: ``
 })
-export class StatusBtnComponent implements  OnChanges{
+export class StatusBtnComponent implements OnInit{
   @Input() status : string | undefined;
+  @Input() book_id : number | undefined;
   statusText: string = "Want to read";
   showStatuses : boolean = false;
 
   protected readonly farSquareCheck = farSquareCheck;
+  constructor(private bookStateService : BookStatesService) {
+  }
 
-  ngOnChanges() {
+  ngOnInit() {
     if (this.status) {
       if (this.status == 'want to read') {
         this.statusText = "Want to read";
@@ -41,5 +45,33 @@ export class StatusBtnComponent implements  OnChanges{
   handleSetStatus(status : string) {
     this.statusText = status;
     this.showStatuses = false;
+
+    if (status == "Want to read") {
+      this.status = 'to read'
+    }
+    if (status == "Currently reading"){
+      this.status = 'in progress'
+    }
+    if (status == 'Read') {
+      this.status = 'read'
+    }
+
+    if (this.book_id && this.status) {
+      this.addBookToNewStatus(this.book_id, this.status)
+    }
+  }
+
+  addBookToNewStatus(book_id : number, status : string) {
+    this.bookStateService.AddBookToStatus(book_id, status , null, null,null, null).subscribe(
+      {
+        next: (response) => {
+          console.log(response)
+          window.location.reload();
+        },
+        error : (error) => {
+          console.log(error)
+        }
+      }
+    )
   }
 }
