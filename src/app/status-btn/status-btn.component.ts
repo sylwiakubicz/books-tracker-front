476e3ed80cf9768af9888a8880e44255ba721239
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-17 18:32:05 +0200

    Dodano metode 'updateBookToNewStatus'. W zależności, czy książka była wcześniej w jakimś statusie czy nie to wykonywany jest adekwatnie update lub dodanie.

--- src/app/status-btn/status-btn.component.ts
+++ src/app/status-btn/status-btn.component.ts
@@ -14,17 +14,18 @@ import {BookStatesService} from "../../services/BookStatesService";
   templateUrl: './status-btn.component.html',
   styles: ``
 })
-export class StatusBtnComponent implements OnInit{
+export class StatusBtnComponent implements OnChanges{
   @Input() status : string | undefined;
   @Input() book_id : number | undefined;
   statusText: string = "Want to read";
   showStatuses : boolean = false;
 
   protected readonly farSquareCheck = farSquareCheck;
+
   constructor(private bookStateService : BookStatesService) {
   }
 
-  ngOnInit() {
+  ngOnChanges() {
     if (this.status) {
       if (this.status == 'want to read') {
         this.statusText = "Want to read";
@@ -46,6 +47,26 @@ export class StatusBtnComponent implements OnInit{
     this.statusText = status;
     this.showStatuses = false;
 
+    if (this.status) {
+      console.log("stat1 " + this.status)
+      this.manageStatuses(status)
+      console.log("stat2 " + this.status)
+
+      if (this.book_id) {
+        this.updateBookToNewStatus(this.book_id, this.status)
+      }
+
+    } else {
+      console.log("stat3 " + this.status)
+      this.manageStatuses(status)
+      console.log("stat4 " + this.status)
+      if (this.book_id && this.status) {
+        this.addBookToNewStatus(this.book_id, this.status)
+      }
+    }
+  }
+
+  manageStatuses (status :string) {
     if (status == "Want to read") {
       this.status = 'to read'
     }
@@ -54,10 +75,6 @@ export class StatusBtnComponent implements OnInit{
     }
     if (status == 'Read') {
       this.status = 'read'
-    }
-
-    if (this.book_id && this.status) {
-      this.addBookToNewStatus(this.book_id, this.status)
     }
   }
 
@@ -74,4 +91,19 @@ export class StatusBtnComponent implements OnInit{
       }
     )
   }
+
+  updateBookToNewStatus(book_id : number, status : string) {
+    this.bookStateService.UpdateBookToStatus(book_id, status , null, null,null, null).subscribe(
+      {
+        next: (response) => {
+          console.log(response)
+          window.location.reload();
+        },
+        error : (error) => {
+          console.log(error)
+        }
+      }
+    )
+  }
+
 }
