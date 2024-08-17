6df3a5616d4e85d1abb6c7c078e9dc5a2dae70ce
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-17 16:59:55 +0200

    Dodano metodę AddBookToStatus oraz interfejs BookStateData
    - Wprowadzono metodę `AddBookToStatus`, umożliwiającą dodanie stanu książki poprzez wysyłanie danych na endpoint.
    - Zdefiniowano interfejs `BookStateData` do modelowania danych stanu książki, takich jak status, aktualna strona, ocena oraz daty.

--- src/services/BookStatesService.ts
+++ src/services/BookStatesService.ts
@@ -11,6 +11,8 @@ import {map} from "rxjs/operators";
 export class BookStatesService {
   constructor(private http: HttpClient) {}
 
+  bookStateData : BookStateData | undefined
+
   CheckIfExistAndThenGet(book_id : number) :Observable<any> {
     return this.http.get(`http://localhost:8080/bookstate/exist/${book_id}`, { withCredentials: true }).pipe(
       map((response) => {
@@ -19,6 +21,30 @@ export class BookStatesService {
     );
   }
 
+  AddBookToStatus(book_id : number, status : string, rate :number, currentPage : number, startDate : Date, endDate : Date  ) : Observable<any> {
+    this.bookStateData = {
+      status : status,
+      currentPage : currentPage,
+      rate : rate,
+      startDate : new Date(startDate).toJSON(),
+      endDate : new Date(endDate).toJSON()
+    }
+
+    return this.http.post(`http://localhost:8080/bookstate/create/${book_id}`, this.bookStateData, { withCredentials: true }).pipe(
+      map((response) => {
+        return response;
+      })
+    )
+  };
+
+}
+
+export interface BookStateData {
+  status : string,
+  currentPage: number,
+  rate : number,
+  startDate : string,
+  endDate : string
 }
 
 
