dba5494d531956711d3adc4668fa30f66a2f1191
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-17 18:00:40 +0200

    Dodano metodę 'UpdateBookToStatus'.

--- src/services/BookStatesService.ts
+++ src/services/BookStatesService.ts
@@ -37,6 +37,22 @@ export class BookStatesService {
     )
   };
 
+  UpdateBookToStatus(book_id: number, status: string, rate: number | null, currentPage: number | null, startDate: Date | null, endDate: Date | null) : Observable<any> {
+    this.bookStateData = {
+      status : status,
+      currentPage : currentPage,
+      rate : rate,
+      startDate : startDate ? new Date(startDate).toJSON() : null,
+      endDate : endDate ? new Date(endDate).toJSON() : null
+    }
+
+    return this.http.put(`http://localhost:8080/bookstate/update/${book_id}`, this.bookStateData, { withCredentials: true }).pipe(
+      map((response) => {
+        return response;
+      })
+    )
+  };
+
 }
 
 export interface BookStateData {
