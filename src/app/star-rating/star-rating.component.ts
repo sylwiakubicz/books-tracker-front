4d3518a8aaa4f3ad50415863280f087869ab9b96
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-18 16:09:42 +0200

    Dodano komponent app-star-rating do szablonu w komponencie BookDetails, przekazując do niego rating, book_id, startDate, endDate. Dodano konstruktor, metodę updateBookToNewStatus, oraz jej wywołanie w metodzie setRating w komponencie StarRatingComponent.

--- src/app/star-rating/star-rating.component.ts
+++ src/app/star-rating/star-rating.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, input} from '@angular/core';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {faStar as farStar} from "@fortawesome/free-regular-svg-icons/faStar";
 import {faStar} from "@fortawesome/free-solid-svg-icons/faStar";
+import {BookStatesService} from "../../services/BookStatesService";
 
 @Component({
   selector: 'app-star-rating',
@@ -15,8 +16,30 @@ export class StarRatingComponent {
   protected readonly faStar = faStar;
 
   @Input() rating : number | undefined;
+  @Input() startDate : Date | undefined;
+  @Input() endDate : Date | undefined;
+  @Input() book_id : number | undefined;
+
+  constructor(private bookStateService : BookStatesService) {
+  }
 
   setRating (value : number) {
     this.rating = value;
+    if (this.book_id)
+    this.updateBookToNewStatus(this.book_id, "Read", this.rating, this.startDate ? this.startDate : null, this.endDate ? this.endDate : null )
   }
+
+  updateBookToNewStatus(book_id : number, status : string, rate : number | null, startDate : Date | null, endDate : Date | null) {
+    this.bookStateService.UpdateBookToStatus(book_id, status , rate, null,startDate, endDate).subscribe(
+      {
+        next: (response) => {
+          window.location.reload();
+        },
+        error : (error) => {
+          console.log(error)
+        }
+      }
+    )
+  }
+
 }
