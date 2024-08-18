927f484e95ecd80954a31e1911c6ca59219feece
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-18 11:31:46 +0200

    Zaktualizowano metodę `saveData` do obsługi zmian statusu książki i integracji z backendem

--- src/app/book-state-details/book-state-details.component.ts
+++ src/app/book-state-details/book-state-details.component.ts
@@ -3,6 +3,7 @@ import {StarRatingComponent} from "../star-rating/star-rating.component";
 import {StatusBtnComponent} from "../status-btn/status-btn.component";
 import { DatePipe } from '@angular/common';
 import {FormsModule} from "@angular/forms";
+import {BookStatesService} from "../../services/BookStatesService";
 
 @Component({
   selector: 'app-book-state-details',
@@ -21,7 +22,12 @@ export class BookStateDetailsComponent {
   @Input() startDate : Date | undefined;
   @Input() endDate : Date | undefined;
   @Input() currentPage : number | undefined;
+  @Input() book_id : number | undefined;
+  @Input() status : string | undefined;
   hideSaveBtn : boolean = true;
+
+  constructor(private bookStateService : BookStatesService) {
+  }
 
   onCurrentPageChange(newVal : number) {
     this.hideSaveBtn = false;
@@ -48,7 +54,36 @@ export class BookStateDetailsComponent {
   }
 
   saveData() {
+    if (this.startDate && this.endDate) {
+      this.status = "Read"
+    }
+    if (this.startDate && !this.endDate) {
+      this.status = "Currently reading"
+    }
+    else {
+      this.status = "Want to read"
+    }
+
+    console.log(this.status)
+    console.log(this.book_id)
+    if (this.book_id && this.status) {
+      this.updateBookToNewStatus(this.book_id, this.status, this.currentPage ? this.currentPage : null, this.startDate ? this.startDate : null, this.endDate ? this.endDate : null)
+    }
     this.hideSaveBtn = true;
+
   }
 
+  updateBookToNewStatus(book_id : number, status : string, currentPage : number | null, startDate : Date | null, endDate : Date | null) {
+    this.bookStateService.UpdateBookToStatus(book_id, status , null, currentPage,startDate, endDate).subscribe(
+      {
+        next: (response) => {
+          console.log("uruchamiam?")
+          window.location.reload();
+        },
+        error : (error) => {
+          console.log(error)
+        }
+      }
+    )
+  }
 }
