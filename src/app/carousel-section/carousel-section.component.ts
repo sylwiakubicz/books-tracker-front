e34d07d8ad035fd75f64da06ac99999f0ec32e2c
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-19 22:56:11 +0200

    Dodano prawy i lewy przycisk galerii.

--- src/app/carousel-section/carousel-section.component.ts
+++ src/app/carousel-section/carousel-section.component.ts
@@ -1,17 +1,25 @@
 import {Component, Input} from '@angular/core';
 import {CarouselItemComponent} from "../carousel-item/carousel-item.component";
 import {NgClass} from "@angular/common";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faAngleLeft} from "@fortawesome/free-solid-svg-icons/faAngleLeft";
+import {faAngleRight} from "@fortawesome/free-solid-svg-icons/faAngleRight";
+
 
 @Component({
   selector: 'app-carousel-section',
   standalone: true,
   imports: [
     CarouselItemComponent,
-    NgClass
+    NgClass,
+    FaIconComponent
   ],
   templateUrl: './carousel-section.component.html',
   styleUrl: `./carousel-section.component.css`
 })
 export class CarouselSectionComponent {
   @Input() title : string = ''
+
+  protected readonly faAngleLeft = faAngleLeft;
+  protected readonly faAngleRight = faAngleRight;
 }
