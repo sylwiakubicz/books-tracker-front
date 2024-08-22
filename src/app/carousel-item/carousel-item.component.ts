45b45de4cb948898f59fd172e839ff6078b672d9
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-22 22:09:20 +0200

    Usuń hover, gdy jest uruchomiona metoda dragging.

--- src/app/carousel-item/carousel-item.component.ts
+++ src/app/carousel-item/carousel-item.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-carousel-item',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './carousel-item.component.html',
   styles: ``
@@ -14,6 +16,7 @@ export class CarouselItemComponent {
   @Input() bookTitle : string | undefined;
   @Input() bookUrl : string | undefined = '';
   @Input() id : number | undefined = 0;
+  @Input() dragging : boolean | undefined = false;
 
   url : string = "https://drive.google.com/thumbnail?id=";
 }
