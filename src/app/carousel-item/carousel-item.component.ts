98b51d6f1ed785a426c774a45f2b2d1708893ed7
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-22 22:01:25 +0200

    Dodano przekierowanie na strone o książce z konkretnym id po kliknięciu na carousel-item.

--- src/app/carousel-item/carousel-item.component.ts
+++ src/app/carousel-item/carousel-item.component.ts
@@ -1,15 +1,19 @@
 import {Component, Input} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-carousel-item',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './carousel-item.component.html',
   styles: ``
 })
 export class CarouselItemComponent {
   @Input() bookTitle : string | undefined;
   @Input() bookUrl : string | undefined = '';
+  @Input() id : number | undefined = 0;
 
   url : string = "https://drive.google.com/thumbnail?id=";
 }
