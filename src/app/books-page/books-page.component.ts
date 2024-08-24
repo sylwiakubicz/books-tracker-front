927f14dbce15a03f6d55172301ca9259b6d16c2f
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-24 19:22:12 +0200

    Dodano do books_page komponenty z których będzie tworzony.

--- src/app/books-page/books-page.component.ts
+++ src/app/books-page/books-page.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BannerComponent} from "../banner/banner.component";
+import {SearchBooksPanelComponent} from "../search-books-panel/search-books-panel.component";
+import {BooksService} from "../../services/BooksService";
+import {Book} from "../home-page/home-page.component";
+import {BookCardComponent} from "../book-card/book-card.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-books-page',
   standalone: true,
   imports: [
-    BannerComponent
+    BannerComponent,
+    SearchBooksPanelComponent,
+    BookCardComponent,
+    NgForOf
   ],
   templateUrl: './books-page.component.html',
   styles: ``
 })
-export class BooksPageComponent {
+export class BooksPageComponent implements OnInit{
+  booksData : Book[] | undefined;
+
+  constructor(private booksService : BooksService) {
+  }
+
+  ngOnInit() {
+    this.getAllBooks();
+  }
+
+  getAllBooks() {
+    this.booksService.GetAllBooks().subscribe({
+      next: (response) => {
+        console.log("Test successful:", response);
+        this.booksData = response.content;
+        console.log(this.booksData)
+      },
+      error: (error) => {
+        console.error("Test failed:", error);
+      }
+    })
+  }
 
 }
