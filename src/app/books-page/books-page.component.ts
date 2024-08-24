1300fff3d38a7035bd239172a47d5a5dbbace457
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-24 19:08:04 +0200

    Dodano komponenty, z których budowana będzie podstrona do wyszukiwania, filtrowania i przeglądania ksiażek.

---
+++ src/app/books-page/books-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './books-page.component.html',
+  styles: ``
+})
+export class BooksPageComponent {
+
+}
---
+++ src/app/search-books-panel/search-books-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-books-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-books-panel.component.html',
+  styles: ``
+})
+export class SearchBooksPanelComponent {
+
+}
