b567b9126df40c7ab800ab092bced49d39f0c56a
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-24 19:09:58 +0200

    Dodano BooksPage do routingu.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import {HomePageComponent} from "./home-page/home-page.component";
 import {MyBooksPageComponent} from "./my-books-page/my-books-page.component";
 import {BookPageComponent} from "./book-page/book-page.component";
 import {NavbarComponent} from "./navbar/navbar.component";
+import {BooksPageComponent} from "./books-page/books-page.component";
 
 export const routes: Routes = [
   {path: "", component: HomePageComponent},
+  {path: "books", component: BooksPageComponent},
   {path: "mybooks", component: MyBooksPageComponent},
   {path: "book/:id", component: BookPageComponent},
   {path: 'login', component: LoginPageComponent, title:"Sign in"},
