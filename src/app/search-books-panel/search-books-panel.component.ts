42bf2b0b3759465b9161e9ec8dec1bad55185551
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-25 23:23:02 +0200

    W search-books-panel pobrano dane o gatunkach i wyepłniono nimi opcje w selectcie.

--- src/app/search-books-panel/search-books-panel.component.ts
+++ src/app/search-books-panel/search-books-panel.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { NgForOf } from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
+import {GenresService} from "../../services/GenresService";
+
 
 @Component({
   selector: 'app-search-books-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent,
+    NgForOf,
+    HttpClientModule
+  ],
+  providers: [],
   templateUrl: './search-books-panel.component.html',
   styles: ``
 })
-export class SearchBooksPanelComponent {
 
+export class SearchBooksPanelComponent implements OnInit{
+  protected readonly faSearch = faSearch;
+
+  constructor(private genresService : GenresService) {
+  }
+
+  genresList : Genres[] = [];
+
+  ngOnInit() {
+    this.getAllGenres()
+  }
+
+  getAllGenres() {
+    this.genresService.GetAllGenres().subscribe({
+      next: (response) => {
+        this.genresList = response
+      },
+      error: (error) => {
+        console.error("Test failed:", error);
+      }
+    })
+  }
+
+}
+
+export interface Genres {
+  id : number
+  name : string
 }
