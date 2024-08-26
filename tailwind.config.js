e0fac1bd68db81ac2aabe682fc4ddd6687340416
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-26 20:09:12 +0200

    Wstawiono docelowy układ baneru wraz z jego responsywnością.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const url = require("node:url");
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
@@ -20,6 +21,9 @@ module.exports = {
         'akcent-color': "#4F6F52",
         "additional-color": '#1A4D2E'
       },
+      backgroundImage: {
+        'baner-tlo': "url('/assets/images/baner_tlo1.png')",
+      }
     },
   },
   plugins: [],
