a5deadb72ec42211b8cb335b128d5ac050f4f6d5
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-29 19:52:01 +0200

    feat: dodanie komponentu MyBookCard na podstawie istniejącego BookCard
    - Utworzono nowy komponent `MyBookCard`, który jest oparty na istniejącym `BookCard`.
    - Komponent `MyBookCard` obecnie ma identyczną strukturę i funkcjonalność jak `BookCard`, co stanowi bazę do dalszych modyfikacji specyficznych dla strony "Moje książki".

--- src/app/mybook-card/mybook-card.component.ts
+++ src/app/mybook-card/mybook-card.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {StatusIconComponent} from "../status-icon/status-icon.component";
+import {RouterLink} from "@angular/router";
+import {Author} from "../home-page/home-page.component";
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+
 
 @Component({
   selector: 'app-mybook-card',
   standalone: true,
-  imports: [],
+  imports: [
+    StatusIconComponent,
+    RouterLink,
+    FaIconComponent
+  ],
   templateUrl: './mybook-card.component.html',
   styles: ``
 })
-export class MybookCardComponent {
+export class MybookCardComponent implements OnInit{
+  @Input() title: string = '';
+  @Input() authors: Author[] = [];
+  @Input() covering: string = '';
+  @Input() description: string = '';
+  @Input() id : number = 0;
+  url : string = "https://drive.google.com/thumbnail?id=";
+  authorsNames: string = "";
+
+
+  ngOnInit() {
+    this.manageReceivedData()
+  }
+
+  manageReceivedData() {
+    this.url = this.url + this.covering;
+
+    this.authorsNames = this.authors?.map((author: Author) =>
+      `${author.name} ${author.surname}`
+    ).join(', ') || '';
+  }
+
+
+  protected readonly faArrowRight = faArrowRight;
 
 }
