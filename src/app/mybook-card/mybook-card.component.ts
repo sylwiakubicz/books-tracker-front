e8a49fee299646ae4771caaaefb6d1d818e4bede
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-29 19:26:45 +0200

    feat: utworzenie komponentów MyBookCard i SearchMyBooksPanel
    - Utworzono nowy komponent `MyBookCard`, który będzie odpowiedzialny za wyświetlanie książek na podstronie "Moje książki".
    - Utworzono nowy komponent `SearchMyBooksPanel`, który będzie odpowiedzialny za zarządzanie wyszukiwaniem i filtrowaniem książek na podstronie "Moje książki".

---
+++ src/app/mybook-card/mybook-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mybook-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './mybook-card.component.html',
+  styles: ``
+})
+export class MybookCardComponent {
+
+}
---
+++ src/app/search-mybooks-panel/search-mybooks-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-mybooks-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-mybooks-panel.component.html',
+  styles: ``
+})
+export class SearchMybooksPanelComponent {
+
+}
