428e5ed6871e76139b430e8b14dd94d560eeb0e7
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-31 17:17:13 +0200

    Dodano odliczanie i automatyczne przekierowanie na stronę główną po 10 sekundach na stronie 404.
    - Dodano właściwość `timer` w `PageNotfoundComponent`, która przechowuje czas do przekierowania.
    - Zaimplementowano metodę `startCountdown`, która odlicza od 10 do 0, aktualizując `timer` co sekundę.
    - Po zakończeniu odliczania użytkownik jest automatycznie przekierowywany na stronę główną (`/`).
    - Zaktualizowano `PageNotfoundComponent`, aby wywoływać `startCountdown` podczas inicjalizacji komponentu.

--- src/app/page-notfound/page-notfound.component.ts
+++ src/app/page-notfound/page-notfound.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FooterComponent} from "../footer/footer.component";
+import {NavbarComponent} from "../navbar/navbar.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-page-notfound',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterComponent,
+    NavbarComponent
+  ],
   templateUrl: './page-notfound.component.html',
   styles: ``
 })
-export class PageNotfoundComponent {
+export class PageNotfoundComponent implements OnInit{
+  timer : number = 10;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.startCountdown();
+  }
+
+  startCountdown() {
+    const interval = setInterval(() => {
+      this.timer -= 1;
+      if (this.timer === 0) {
+        clearInterval(interval);
+        this.router.navigate(['/']);
+      }
+    }, 1000);
+  }
 
 }
