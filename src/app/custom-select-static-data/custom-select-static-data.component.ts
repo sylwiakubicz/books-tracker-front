52e777434e4a74adbb3c3101fc55a404a812ba1c
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-31 23:10:24 +0200

    Dodano zamykanie menu sortowania po kliknięciu poza jego obszarem custom-select-static-data komponentu.
    - Dodano `@HostListener` do nasłuchiwania kliknięć na dokument w celu zamknięcia menu sortowania, gdy użytkownik kliknie poza obszarem przycisku sortowania i rozwijanej listy.
    - Zaktualizowano szablon HTML, aby dodać referencję `#dropdown` do głównego kontenera, co umożliwia identyfikację kliknięć wewnątrz elementu.

--- src/app/custom-select-static-data/custom-select-static-data.component.ts
+++ src/app/custom-select-static-data/custom-select-static-data.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, Output, ViewChild} from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {NgClass} from "@angular/common";
 
@@ -15,6 +15,8 @@ import {NgClass} from "@angular/common";
 export class CustomSelectStaticDataComponent {
   @Output() statusSelected = new EventEmitter<string>
 
+  @ViewChild('dropdown', { static: false }) dropdown!: ElementRef;
+
   isShow : Boolean = false;
   selectedOption: string = 'Status';
 
@@ -26,6 +28,13 @@ export class CustomSelectStaticDataComponent {
     this.selectedOption = option;
     this.isShow = false;
     this.statusSelected.emit(option)
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (this.isShow && this.dropdown && !this.dropdown.nativeElement.contains(event.target)) {
+      this.isShow = false;
+    }
   }
 
 }
