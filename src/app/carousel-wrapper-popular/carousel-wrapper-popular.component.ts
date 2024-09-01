b1a1680bfd16d45069113aeb3cf334711c785867
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-01 22:31:38 +0200

    Przeniesiono logikę pobierania danych do komponentu `CarouselWrapperPopularComponent` oraz dodano obsługę zmiany gatunku: - Zmienna `booksData` jest teraz inicjalizowana bezpośrednio w `CarouselWrapperPopularComponent`. - Dodano metodę `onGenreChange`, która uruchamia się po wyemitowaniu nowego gatunku przez `CarouselMenuComponent`. - Metoda `getNewestBooks` została zaimplementowana w `CarouselWrapperPopularComponent`, aby pobierać odpowiednie dane na podstawie wybranego gatunku.

--- src/app/carousel-wrapper-popular/carousel-wrapper-popular.component.ts
+++ src/app/carousel-wrapper-popular/carousel-wrapper-popular.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CarouselSectionComponent} from "../carousel-section/carousel-section.component";
 import {CarouselTitleComponent} from "../carousel-title/carousel-title.component";
 import {CarouselMenuComponent} from "../carousel-menu/carousel-menu.component";
 import {Book} from "../home-page/home-page.component";
+import {BooksService} from "../../services/BooksService";
 
 @Component({
   selector: 'app-carousel-wrapper-popular',
@@ -15,6 +16,28 @@ import {Book} from "../home-page/home-page.component";
   templateUrl: './carousel-wrapper-popular.component.html',
   styles: ``
 })
-export class CarouselWrapperPopularComponent {
-  @Input() booksData : Book[] | undefined;
+export class CarouselWrapperPopularComponent implements OnInit{
+  booksData : Book[] | undefined;
+  genre : string | undefined;
+
+  constructor(private booksService: BooksService) {
+  }
+
+  ngOnInit() {
+    this.getNewestBooks(this.genre ? this.genre : 'fantasy')
+  }
+
+  onGenreChange(genre :string) {
+    this.genre = genre
+    this.getNewestBooks(genre)
+  }
+
+  getNewestBooks(genre :string) {
+    this.booksService.GetNewestBooks(genre).subscribe({
+      next: (response) => {
+        console.log(response)
+        this.booksData = response;
+      }
+    })
+  }
 }
