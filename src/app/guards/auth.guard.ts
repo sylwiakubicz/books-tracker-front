b874388f9000479fe083970e7b5f01c056cc38de
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-01 11:53:09 +0200

    Wprowadzenie autoryzacji z użyciem CanActivateFn w Angularze.
    - Zaktualizowano `auth.guard.ts`, aby wykorzystać funkcję `CanActivateFn` do kontrolowania dostępu do routingu w aplikacji.
    - Wstrzyknięto `AuthService` i `Router` do guardu, aby móc pobrać rolę użytkownika oraz nawigować pomiędzy stronami.
    - Dodano logikę sprawdzającą, czy użytkownik jest zalogowany oraz czy posiada odpowiednią rolę do uzyskania dostępu do danej ścieżki.
    - Użytkownik zostaje przekierowany na stronę główną, jeśli próbuje uzyskać dostęp do stron `login` lub `register` będąc już zalogowanym.
    - Guard sprawdza również, czy użytkownik ma wymaganą rolę, aby uzyskać dostęp do określonych stron; w przypadku braku uprawnień następuje przekierowanie na stronę główną.

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import {AuthService} from "../../services/AuthService";
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.GetUserRole().pipe(
+    map(role => {
+      const isLoggedIn = !!role;
+
+      if (isLoggedIn && (route.routeConfig?.path === 'login' || route.routeConfig?.path === 'register')) {
+        router.navigate(['/']);
+        return false;
+      }
+
+      const requiredRole = route.data['role'] as string;
+      if (requiredRole && role !== requiredRole) {
+        router.navigate(['/']);
+        return false;
+      }
+
+      return true;
+    })
+  );
+};
