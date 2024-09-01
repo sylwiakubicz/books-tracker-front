604979d4e0f532bd3a1042937266e60bada1f3e2
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-01 11:07:03 +0200

    Utworzono AdminPageComponent i dodano go do routingu.

---
+++ src/app/admin-page/admin-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-page.component.html',
+  styles: ``
+})
+export class AdminPageComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {MyBooksPageComponent} from "./my-books-page/my-books-page.component";
 import {BookPageComponent} from "./book-page/book-page.component";
 import {BooksPageComponent} from "./books-page/books-page.component";
 import {PageNotfoundComponent} from "./page-notfound/page-notfound.component";
+import {AdminPageComponent} from "./admin-page/admin-page.component";
 
 export const routes: Routes = [
   {path: "", component: HomePageComponent},
@@ -14,6 +15,7 @@ export const routes: Routes = [
   {path: "book/:id", component: BookPageComponent},
   {path: 'login', component: LoginPageComponent, title:"Sign in"},
   {path: 'register', component: RegisterPageComponent, title: "Register"},
+  {path: 'admin', component: AdminPageComponent},
   {path: '**', component: PageNotfoundComponent}
 ];
 
