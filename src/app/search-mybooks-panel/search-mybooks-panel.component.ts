81b0a930e40e9a6c8fe931ee20c888ed2916c7c7
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-03 19:58:48 +0200

    Zmieniono pageSize z 10 na 20 w search-mybooks-komponencie.

--- src/app/search-mybooks-panel/search-mybooks-panel.component.ts
+++ src/app/search-mybooks-panel/search-mybooks-panel.component.ts
@@ -46,7 +46,7 @@ export class SearchMybooksPanelComponent implements OnInit{
   bookStateData : BookState[] = [];
 
   totalItems = 0;
-  pageSize = 10;
+  pageSize = 20;
   currentPage = 1;
   search : string = '';
   selectedGenre : string = '';
