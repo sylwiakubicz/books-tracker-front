8c39caae042e8a6836d2a8352202503d23017b7b
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-04 22:29:50 +0200

    Refaktoryzacja admin-menu komponentu tak aby korzystał z bardziej semantycznych tagów.

---
+++ src/app/admin-books-form/admin-books-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-books-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-books-form.component.html',
+  styles: ``
+})
+export class AdminBooksFormComponent {
+
+}
---
+++ src/app/admin-books-table/admin-books-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-books-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-books-table.component.html',
+  styles: ``
+})
+export class AdminBooksTableComponent {
+
+}
