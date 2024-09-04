5638754abb062267f3955dc54c7aa2d69dcada4c
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-04 23:24:04 +0200

    Dodano nową strukturę dla strony zarządzania kontami (`admin-accounts-page.component.html`) oraz tabeli kont (`admin-accounts-table.component.html`). - Utworzono sekcję nawigacyjną z przyciskami filtrowania użytkowników (All, Administration, Users) z dynamicznym podświetlaniem aktywnego filtra za pomocą klasy `ngClass`. - Dodano przycisk "Add new" w sekcji header na stronie zarządzania kontami. - Zaimplementowano filtrację widoku kont poprzez zmienną `activeFilter`, której domyślna wartość to 'all'. - Zaktualizowano tabelę z listą kont, dodając kolumny `Username`, `Email`, `Password`, oraz `Role`.

--- src/app/admin-accounts-table/admin-accounts-table.component.ts
+++ src/app/admin-accounts-table/admin-accounts-table.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-admin-accounts-table',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './admin-accounts-table.component.html',
   styles: ``
 })
 export class AdminAccountsTableComponent {
+
+  activeFilter : string = 'all'
 
 }
