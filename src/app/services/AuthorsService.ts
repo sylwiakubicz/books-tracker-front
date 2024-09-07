ad7fca4f477aaa67fd75d5e0e4ed7001663e5f92
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-07 23:35:24 +0200

    Dodano metody AddAuthor i UpdateAuthor w AuthorsService
    - Utworzono metodę `AddAuthor`, która wysyła żądanie POST na endpoint `/authors` z danymi imienia i nazwiska autora.
    - Zaimplementowano metodę `UpdateAuthor`, która wysyła żądanie PUT z obiektem `author` na endpoint `/authors` w celu aktualizacji istniejącego autora.

--- src/app/services/AuthorsService.ts
+++ src/app/services/AuthorsService.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
+import {Author} from "../user-front/home-page/home-page.component";
 
 @Injectable({
   providedIn: 'root',
@@ -23,6 +24,28 @@ export class AuthorsService {
       httpParams = httpParams.set('search', params.search);
     }
     return this.http.get("http://localhost:8080/authors", { params: httpParams,withCredentials: true}).pipe(
+      map(response => {
+        return response
+      })
+    )
+  }
+
+  AddAuthor(authorsName: string, authorsSurname: string) : Observable<any> {
+    return this.http.post(
+      "http://localhost:8080/authors",
+      {
+        name: authorsName,
+        surname: authorsSurname
+      },
+      {withCredentials: true}).pipe(
+      map(response => {
+        return response
+      })
+    )
+  }
+
+  UpdateAuthor(author : Author) : Observable<any> {
+    return this.http.put("http://localhost:8080/authors", author, {withCredentials: true}).pipe(
       map(response => {
         return response
       })
