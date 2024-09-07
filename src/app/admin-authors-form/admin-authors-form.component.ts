829280ddb8524114e07649ed72f4c9ec6b6cc501
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-07 08:18:13 +0200

    Utworzono formularz AdminAuthorsFormComponent do zarządzania autorami
    - Utworzono komponent AdminAuthorsFormComponent, który zawiera formularz do dodawania i edycji danych autorów.
    - Formularz zawiera pola: Id, Name, Surname, z odpowiednią walidacją i stylizacją.

---
+++ src/app/admin-authors-form/admin-authors-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-authors-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-authors-form.component.html',
+  styles: ``
+})
+export class AdminAuthorsFormComponent {
+
+}
