2bafad0d2b0ef1a0ad3d87d8f01de6104d887177
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-08 00:02:38 +0200

    feat: Dodano metody AddAuthor, UpdateAuthor oraz OnInit w AdminAuthorsFormComponent
    - Dodano metodę `AddAuthor` do tworzenia nowych autorów, która pobiera imię i nazwisko autora i przekazuje je do serwisu AuthorsService.
    - Dodano metodę `UpdateAuthor`, która aktualizuje dane autora na podstawie przesłanych wartości.
    - Zaimplementowano metodę `ngOnInit`, która sprawdza, czy istnieje przekazany id autora, a następnie pobiera dane autora z serwisu i uzupełnia pola formularza imieniem i nazwiskiem.

--- src/app/admin-front/admin-authors-form/admin-authors-form.component.ts
+++ src/app/admin-front/admin-authors-form/admin-authors-form.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {AuthorsService} from "../../services/AuthorsService";
+import {map} from "rxjs/operators";
+import {Router} from "@angular/router";
+import {Author} from "../../user-front/home-page/home-page.component";
 
 @Component({
   selector: 'app-admin-authors-form',
@@ -7,6 +11,56 @@ import { Component } from '@angular/core';
   templateUrl: './admin-authors-form.component.html',
   styles: ``
 })
-export class AdminAuthorsFormComponent {
+export class AdminAuthorsFormComponent implements OnInit{
 
+  constructor(private authorService : AuthorsService, private router : Router) {
+  }
+
+  @Input() id : number = 0
+  authorName : string = ''
+  authorSurname : string = ''
+
+  ngOnInit() {
+    if (this.id !== 0) {
+      this.authorService.GetAuthorById(this.id).subscribe({
+        next: (author: Author) => {
+          this.authorName = author.name;
+          this.authorSurname = author.surname;
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  }
+
+  AddAuthor() {
+    this.authorService.AddAuthor(this.authorName, this.authorSurname).subscribe({
+        next: (response) => {
+          this.router.navigate(['/admin/library/authors']);
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      }
+    )
+  }
+
+  UpdateAuthor() {
+    const updatedAuthor: Author = {
+      authorId: this.id,
+      name: this.authorName,
+      surname: this.authorSurname
+    };
+
+    this.authorService.UpdateAuthor(updatedAuthor).subscribe({
+        next: (response) => {
+          this.router.navigate(['/admin/library/authors']);
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      }
+    )
+  }
 }
