46eb3c13a32e8f918d8a70ad84a363988e176bb6
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-07 15:00:28 +0200

    Dodano metodę GetAllUsers w komponencie AdminAccountsTableComponent
    - Utworzono metodę `getAllUsers`, która pobiera dane użytkowników z serwera na podstawie roli, numeru strony oraz rozmiaru strony.
    - Zaimplementowano dynamiczne przypisanie danych do zmiennej `userData` na podstawie otrzymanej odpowiedzi.
    - Dodano inicjalizację metody `getAllUsers` w metodzie `ngOnInit`, aby automatycznie pobierać dane po załadowaniu komponentu.
    - Utworzono interfejsy `User` oraz `Role` do reprezentacji struktury danych użytkowników w aplikacji.

--- src/app/admin-front/admin-accounts-table/admin-accounts-table.component.ts
+++ src/app/admin-front/admin-accounts-table/admin-accounts-table.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import {NgClass} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {NgClass, NgForOf} from "@angular/common";
 import {AdminEditBtnComponent} from "../admin-edit-btn/admin-edit-btn.component";
 import {AdminDeleteBtnComponent} from "../admin-delete-btn/admin-delete-btn.component";
+import {AuthService} from "../../services/AuthService";
 
 @Component({
   selector: 'app-admin-accounts-table',
@@ -9,13 +10,58 @@ import {AdminDeleteBtnComponent} from "../admin-delete-btn/admin-delete-btn.comp
   imports: [
     NgClass,
     AdminEditBtnComponent,
-    AdminDeleteBtnComponent
+    AdminDeleteBtnComponent,
+    NgForOf
   ],
   templateUrl: './admin-accounts-table.component.html',
   styles: ``
 })
-export class AdminAccountsTableComponent {
+export class AdminAccountsTableComponent implements OnInit{
 
-  activeFilter : string = 'all'
+  constructor(private authService : AuthService) {
+  }
+  activeFilter : string = ''
+  totalItems = 0;
+  pageSize = 20;
+  currentPage = 1;
+  isLoading : boolean = true
+  userData : User[] = []
+
+
+  ngOnInit() {
+    this.getAllUsers()
+  }
+
+  getAllUsers() {
+    this.authService.GetAllUsers({
+      role: 'ROLE_USER',
+      page: this.currentPage - 1,
+      size: this.pageSize
+    }).subscribe({
+      next: (response) => {
+        this.userData = response.content
+        console.log(this.userData)
+        this.totalItems = response.totalElements
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.log("Failed: ", error)
+        this.isLoading = false
+      }
+    })
+  }
+}
+
+export interface User {
+  userId: number
+  username : string
+  email : string
+  role : Role
+  creationDate: string
+}
+
+export interface Role {
+  roleId : number
+  role : string
 
 }
