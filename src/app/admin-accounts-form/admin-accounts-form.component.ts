8a99c7c76ede3150f1dc9df159d1b860ecd1b6f2
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-07 07:41:15 +0200

    Utworzono formularz AdminAccountsFormComponent do zarządzania kontami użytkowników
    - Dodano formularz w komponencie AdminAccountsFormComponent, który umożliwia edycję i dodawanie użytkowników.
    - Formularz zawiera pola: Id, Username, Email, Password oraz Role, z możliwością wyboru roli (User/Admin).

---
+++ src/app/admin-accounts-form/admin-accounts-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-accounts-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-accounts-form.component.html',
+  styles: ``
+})
+export class AdminAccountsFormComponent {
+
+}
