cdbc66853a6eeafaa2d3509176a5e251651a8d63
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-07 18:22:11 +0200

    Dodano funkcjonalność paginacji i wyszukiwania w tabeli autorów
    - Zaimplementowano komponent `AdminAuthorsTableComponent`, który wyświetla listę autorów z możliwością paginacji i wyszukiwania.
    - Dodano metodę `getAllAuthors`, która pobiera autorów z serwera z uwzględnieniem parametrów takich jak rozmiar strony, bieżąca strona i zapytanie wyszukiwania.
    - Wprowadzono pole wyszukiwania z dwukierunkowym wiązaniem danych (ngModel) oraz obsługą wyszukiwania po naciśnięciu klawisza Enter.
    - Dodano komponent paginacji (`pagination-controls`), który umożliwia przełączanie stron, a także resetowanie scrolla po zmianie strony.
    - Zastosowano komponenty `AdminEditBtnComponent` i `AdminDeleteBtnComponent` dla akcji edycji i usuwania autorów.

--- src/app/admin-front/admin-authors-table/admin-authors-table.component.ts
+++ src/app/admin-front/admin-authors-table/admin-authors-table.component.ts
@@ -1,17 +1,82 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AdminEditBtnComponent} from "../admin-edit-btn/admin-edit-btn.component";
 import {AdminDeleteBtnComponent} from "../admin-delete-btn/admin-delete-btn.component";
+import {AuthorsService} from "../../services/AuthorsService";
+import {Author} from "../../user-front/home-page/home-page.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {LoadingComponent} from "../../user-front/loading/loading.component";
+import {NgxPaginationModule} from "ngx-pagination";
+import {AuthService} from "../../services/AuthService";
+import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-admin-authors-table',
   standalone: true,
   imports: [
     AdminEditBtnComponent,
-    AdminDeleteBtnComponent
+    AdminDeleteBtnComponent,
+    NgIf,
+    LoadingComponent,
+    NgxPaginationModule,
+    NgForOf,
+    FormsModule
   ],
   templateUrl: './admin-authors-table.component.html',
   styles: ``
 })
-export class AdminAuthorsTableComponent {
+export class AdminAuthorsTableComponent implements OnInit{
+  constructor(private authorsService : AuthorsService, private authService : AuthService, private router : Router) {
+  }
 
+  ngOnInit() {
+    this.getAllAuthors()
+  }
+
+  authorsData : Author[] = []
+  search : string = ''
+  isLoading : boolean = true
+  totalItems = 0;
+  pageSize = 20;
+  currentPage = 1;
+
+  getAllAuthors() {
+    this.authorsService.GetAllAuthors({
+      size: this.pageSize,
+      page: this.currentPage - 1,
+      search: this.search
+    }).subscribe({
+      next: (response) => {
+        this.authorsData = response.content
+        this.totalItems = response.totalElements
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.log("Failed: ", error)
+        this.isLoading = false
+      }
+    })
+  }
+
+  pageChanged(page : number) {
+    this.currentPage = page
+    this.authService.GetUserRole().subscribe(role => {
+      if (role === null) {
+        this.router.navigate(['/login']);
+      } else {
+        window.scrollTo(0, 0);
+        this.getAllAuthors();
+      }
+    })
+  }
+
+  handleSearch() {
+    this.authService.GetUserRole().subscribe(role => {
+      if (role === null) {
+        this.router.navigate(['/login']);
+      } else {
+        this.getAllAuthors();
+      }
+    })
+  }
 }
