6ec40f65cdfd635f1fb6b41d6816cdf00275a160
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-08 11:50:20 +0200

    Dodano funkcjonalność usuwania autora w komponencie admin-delete-btn. 1. Zaimplementowano metodę `handleDelete`, która po kliknięciu przycisku usunięcia, sprawdza uprawnienia użytkownika oraz na podstawie `tableName` wywołuje odpowiednią usługę do usuwania. 2. Wprowadzono dynamiczne przekazywanie `id` i `tableName` do komponentu `admin-delete-btn`. 3. Po usunięciu autora następuje przeładowanie strony w celu odświeżenia widoku tabeli autorów.

--- src/app/admin-front/admin-delete-btn/admin-delete-btn.component.ts
+++ src/app/admin-front/admin-delete-btn/admin-delete-btn.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input} from '@angular/core';
+import {AuthorsService} from "../../services/AuthorsService";
+import {AuthService} from "../../services/AuthService";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-admin-delete-btn',
@@ -8,11 +11,31 @@ import {Component, Input} from '@angular/core';
   styles: ``
 })
 export class AdminDeleteBtnComponent {
+  constructor(private authorsService : AuthorsService, private authService : AuthService, private router :Router) {
+  }
 
   @Input() id : number = 0;
+  @Input() tableName : string = ''
 
-  handleDelete(id : number) {
-    console.log("delete " + id)
+  handleDelete() {
+    console.log(this.id)
+    this.authService.GetUserRole().subscribe(role => {
+      if (role === null) {
+        this.router.navigate(['/login']);
+      } else {
+        if (this.tableName === 'authors') {
+          console.log("id" + this.id)
+          this.authorsService.DeleteAuthor(this.id).subscribe({
+            next: () => {
+              location.reload()
+            },
+            error: (error) => {
+              console.log(error)
+            }
+          })
+        }
+      }
+    })
   }
 
 }
